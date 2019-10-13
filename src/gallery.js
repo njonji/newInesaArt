efaa51b936a6a0d6523874904025e327dea0ba77
Veladzic Senahid <losloboss@yahoo.com>
2019-10-13 16:25:34 +0200

    finished gallery

--- src/gallery.js
+++ src/gallery.js
@@ -1,9 +1,23 @@
-const current = document.querySelector("#current");
-const imgs = document.querySelectorAll(".images__image");
+const current = document.querySelector("#current--1");
+const current2 = document.querySelector("#current--2");
+const current3 = document.querySelector("#current--3");
+const current4 = document.querySelector("#current--4");
+const currentHome = document.querySelector("#current");
+const imgs = document.querySelectorAll(".images__image--1");
+const imgs2 = document.querySelectorAll(".images__image--2");
+const imgs3 = document.querySelectorAll(".images__image--3");
+const imgs4 = document.querySelectorAll(".images__image--4");
+const imgsHome = document.querySelectorAll(".images__image");
 const opacity = 0.6;
 
 
 imgs.forEach(img =>  img.addEventListener("click", imgClick));
+imgs2.forEach(img =>  img.addEventListener("click", imgClick2));
+imgs3.forEach(img =>  img.addEventListener("click", imgClick3));
+imgs4.forEach(img =>  img.addEventListener("click", imgClick4));
+imgsHome.forEach(img =>  img.addEventListener("click", imgClickHome));
+
+
 
 //Set first img opacity to active
 imgs[0].style.opacity = opacity;
@@ -21,6 +35,79 @@ function imgClick(e) {
 
     //Remove fade in class after .3s
    setTimeout(() => current.classList.remove("fade-in"),500)
+
+   //Change opacity on clicked image
+
+   e.target.style.opacity = opacity;
+}
+
+function imgClick2(e) {
+
+    //Reset opacity
+    imgs2.forEach(img2 => (img2.style.opacity = 1));
+
+    //Change current image with clicked
+   current2.src = e.target.src;
+
+    //Add fade in class
+   current2.classList.add("fade-in");
+
+    //Remove fade in class after .3s
+   setTimeout(() => current2.classList.remove("fade-in"),500)
+
+   //Change opacity on clicked image
+
+   e.target.style.opacity = opacity;
+}
+function imgClick3(e) {
+
+    //Reset opacity
+    imgs3.forEach(img3 => (img3.style.opacity = 1));
+
+    //Change current image with clicked
+   current3.src = e.target.src;
+
+    //Add fade in class
+   current3.classList.add("fade-in");
+
+    //Remove fade in class after .3s
+   setTimeout(() => current3.classList.remove("fade-in"),500)
+
+   //Change opacity on clicked image
+
+   e.target.style.opacity = opacity;
+}
+function imgClick4(e) {
+
+    //Reset opacity
+    imgs4.forEach(img4 => (img4.style.opacity = 1));
+
+    //Change current image with clicked
+   current4.src = e.target.src;
+
+    //Add fade in class
+   current4.classList.add("fade-in");
+
+    //Remove fade in class after .3s
+   setTimeout(() => current4.classList.remove("fade-in"),500)
+
+   //Change opacity on clicked image
+
+   e.target.style.opacity = opacity;
+}
+function imgClickHome(e) {
+
+    //Reset opacity
+    imgsHome.forEach(imgHome => (imgHome.style.opacity = 1));
+
+    //Change current image with clicked
+   currentHome.src = e.target.src;
+
+    //Add fade in class
+   currentHome.classList.add("fade-in");
+
+    //Remove fade in class after .3s
+   setTimeout(() => currentHome.classList.remove("fade-in"),500)
 
    //Change opacity on clicked image
 
